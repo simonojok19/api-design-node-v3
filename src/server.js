fea84b9037d8eadc63cf8203b2117078db055f5f
simonojok19 <simonojok19@gmail.com>
2020-07-18 11:32:48 +0300

    created the server

--- src/server.js
+++ src/server.js
@@ -12,4 +12,16 @@ app.use(json())
 app.use(urlencoded({ extended: true }))
 app.use(morgan('dev'))
 
-export const start = () => {}
+app.get('/', (req, res) => {
+  res.send({ message: 'Hello world' })
+})
+
+app.post('/', (req, res) => {
+  console.log(res.body)
+  res.send({ message: 'ok' })
+})
+export const start = () => {
+  app.listen(3000, () => {
+    console.log('sever is running')
+  })
+}
